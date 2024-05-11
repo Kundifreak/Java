8eb288cacc7a70344e5e97c5cab07c0755cb9e68
Kundifreak <148829581+Kundifreak@users.noreply.github.com>
2024-05-10 22:37:58 -0700

    Add files via upload

--- GraphCreator.java
+++ GraphCreator.java
@@ -1,3 +1,8 @@
+/* Created by: Nikaansh S.
+ * Last Edited: 5/12/23
+ * This class has two main search algoritms. One to find if two nodes are connected and another to find the least expensive path between two nodes
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -7,234 +12,235 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-	public class GraphCreator implements ActionListener, MouseListener 
-	{
-		
-		JFrame frame = new JFrame();
-		GraphPanel panel = new GraphPanel();
-		JButton nodeB = new JButton("Node");
-		JButton edgeB = new JButton("Edge");
-		JTextField labelsTF = new JTextField("A");
-		JTextField FNode = new JTextField("First");
-		JTextField SNode = new JTextField("Second");
-		JButton connectedB = new JButton("Test Connected");
-		Container west = new Container();
-		Container east = new Container();
-		Container south = new Container();
-		JTextField salesmanstart = new JTextField("A");
-		JButton salesmanB = new JButton("Shortest Path");
-		final int NODE_CREATE = 0;
-		final int EDGE_FIRST = 1;
-		final int EDGE_SECOND = 2;
-				
-		int state = NODE_CREATE;
-		Node first = null;
-		ArrayList<ArrayList<Node>> completed = new ArrayList<ArrayList<Node>>();
-		
-		public GraphCreator() {
-			frame.setSize(800,400);
-			frame.setLayout(new BorderLayout());
-			frame.add(panel, BorderLayout.CENTER);
-			west.setLayout(new GridLayout(3,1));
-			west.add(nodeB);
-			nodeB.addActionListener(this);
-			nodeB.setBackground(Color.GREEN);
-			west.add(edgeB);
-			edgeB.addActionListener(this);
-			edgeB.setBackground(Color.GRAY);
-			west.add(labelsTF);
-			frame.add(west, BorderLayout.WEST);
-			east.setLayout(new GridLayout(3,1));
-			east.add(FNode);
-			east.add(SNode);
-			east.add(connectedB);
-			connectedB.addActionListener(this);
-			frame.add(east, BorderLayout.EAST);
-			panel.addMouseListener(this);
-			south.setLayout(new GridLayout(1,2));
-			south.add(salesmanstart);
-			south.add(salesmanB);
-			salesmanB.addActionListener(this);
-			frame.add(south, BorderLayout.SOUTH);
-			
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
-		}
-		
-		public static void main(String[] args) 
-		{
-			new GraphCreator();
-	
-		}
-	
-		@Override
-		public void mouseClicked(MouseEvent e) 
-		{
-			
-		}
-	
-		@Override
-		public void mousePressed(MouseEvent e) 
-		{
-			
-		}
-	
-		@Override
-		public void mouseReleased(MouseEvent e) 
-		{
-			if(state == NODE_CREATE){
-			panel.addNode(e.getX(), e.getY(), labelsTF.getText());
-			}
-			else if (state == EDGE_FIRST) 
-			{
-				Node n = panel.getNode(e.getX(), e.getY());
-				if (n != null) 
-				{
-					first = n;
-					state = EDGE_SECOND;
-					n.setHighlighted(true);
-				}
-			}
-			else if (state == EDGE_SECOND) 
-			{
-				Node n = panel.getNode(e.getX(), e.getY());
-				if (n != null && !first.equals(n)) 
-				{
-					String s = labelsTF.getText();
-					boolean valid = true;
-					for (int i = 0; i < s.length(); i++) 
-					{
-						if (Character.isDigit(s.charAt(i))==false)
-						{
-							valid = false;
-						}
-						if(valid == true) 
-						{
-							first.setHighlighted(false);
-							panel.addEdge(first, n, labelsTF.getText());
-							first = null;
-							state = EDGE_FIRST;
-						}
-						else 
-						{
-							JOptionPane.showMessageDialog(frame, "Can only have digits in edge labels");
-						}
-					}
-				}
-			}
-			frame.repaint();
-			
-		}
-	
-		@Override
-		public void mouseEntered(MouseEvent e) {}
-	
-		@Override
-		public void mouseExited(MouseEvent e) {}
-	
-		@Override
-		public void actionPerformed(ActionEvent e) 
-		{
-			if (e.getSource().equals(nodeB)) {
-				nodeB.setBackground(Color.GREEN);
-				edgeB.setBackground(Color.GRAY);
-				state = NODE_CREATE;
-			}
-			if (e.getSource().equals(edgeB)) 
-			{
-				nodeB.setBackground(Color.GREEN);
-				edgeB.setBackground(Color.GRAY);
-				state = EDGE_FIRST;
-				panel.stopHighlighting();
-				frame.repaint();
-			}
-			if (e.getSource().equals(connectedB)) 
-			{
-				if (panel.nodeExists(FNode.getText()) == false)
-				{
-					JOptionPane.showMessageDialog(frame,"First node is not in your graph");
-				}
-				else if (panel.nodeExists(SNode.getText()) == false) 
-				{
-					JOptionPane.showMessageDialog(frame,"Second node is not in your graph");
-				}
-				else
-				{
-					Queue queue = new Queue();
-					ArrayList<String> connectedList = new ArrayList<String>();
-					connectedList.add(panel.getNode(FNode.getText()).getLabel());
-					ArrayList<String> edges = panel.getConnectedLabels(FNode.getText());
-					for (int i = 0; i < edges.size(); i++) 
-					{
-						//check each connected node
-						queue.enqueue(edges.get(i));
-					}
-					while(queue.isEmpty()==false) 
-					{
-						String currentNode = queue.dequeue();
-						if (connectedList.contains(currentNode) == false)
-						{
-							connectedList.add(currentNode);
-						}
-						edges = panel.getConnectedLabels(currentNode);
-						for (int i = 0; i < edges.size(); i++) 
-						{
-							if (connectedList.contains(edges.get(i)) == false) 
-							{
-								queue.enqueue(edges.get(i));
-							}
-						}
-					}
-					if (connectedList.contains(SNode.getText())) 
-					{
-						JOptionPane.showMessageDialog(frame, "Connected");
-					}
-					else 
-					{
-						JOptionPane.showMessageDialog(frame, "Not Connected");
-					}
-				}
-			}
-			if (e.getSource().equals(salesmanB)) 
-			{
-				if (panel.getNode(salesmanstart.getText())!= null) {
-					traveling(panel.getNode(salesmanstart.getText()), new ArrayList<Node>(), 0);	
-				}
-				else {
-					JOptionPane.showMessageDialog(frame, "Not a valid starting node");
-				}
-			}
-		
-	
-	}
-		private void traveling(Node n, ArrayList<Node> path, int total) {
-	        path.add(n);
+import org.w3c.dom.NodeList;
 
-	        // If all nodes are visited
-	        if (path.size() == panel.getNodeCount()) {
-	            handleCompletedPath(path, total);
-	        } else {
-	        	ArrayList<Edge> getEdges = panel.getEdges(n);
-	            for (Edge edge : getEdges) {
-	                Node nextNode = edge.getOtherEnd(n);
-	                if (!path.contains(nextNode)) {
-	                    int newTotal = total + Integer.parseInt(edge.getLabel());
-	                    traveling(nextNode, new ArrayList<>(path), newTotal);
-	                }
-	            }
-	        }
-	    }
+public class GraphCreator implements ActionListener, MouseListener{
 
-	    private void handleCompletedPath(ArrayList<Node> path, int total) {
-	        path.add(path.get(0)); // Adding the starting node to complete the cycle
-	        completed.add(new ArrayList<>(path)); // Add a copy of the path to the completed list
-	        // Logic to handle the completed path, like storing the shortest path found so far
-	    }
-	}
+    //Global Variables
+    JFrame frame = new JFrame();
+    GraphPanel panel = new GraphPanel();
+    JButton nodeB = new JButton("Node");
+    JButton edgeB = new JButton("Edge");
+    JTextField labelsTF = new JTextField("a");
+    JTextField firstNode = new JTextField("first");
+    JTextField secondNode = new JTextField("second");
+    JButton connected = new JButton("Test Connected");
+    Container west = new Container();
+    Container east = new Container();
+    Container south = new Container();
+    JTextField salesmanStartTF = new JTextField("A");
+    JButton salesmanB = new JButton("Shortest Path"); 
+    final int NODE_CREATE = 0;
+    final int EDGE_FIRST = 1;
+    final int EDGE_SECOND = 2;
+    int state = NODE_CREATE; 
+    Node first = null;
+    ArrayList<ArrayList<Node>> completed = new ArrayList<ArrayList<Node>>();
+    ArrayList<Integer> totalList = new ArrayList<Integer>();
+
+    public GraphCreator() {
+        //Setting the Frame
+        frame.setSize(800, 600);
+        frame.setLayout(new BorderLayout());
+        frame.add(panel, BorderLayout.CENTER); 
+
+        west.setLayout(new GridLayout(3,1));
+        west.add(nodeB);
+        nodeB.addActionListener(this);
+        nodeB.setBackground(Color.GREEN);
+        west.add(edgeB);
+        edgeB.setBackground(Color.LIGHT_GRAY);
+        edgeB.addActionListener(this);
+        west.add(labelsTF);
+        frame.add(west, BorderLayout.WEST);
+
+        south.setLayout(new GridLayout(1, 2));
+        south.add(salesmanStartTF);
+        south.add(salesmanB);
+        salesmanB.addActionListener(this);
+        frame.add(south, BorderLayout.SOUTH);
+ 
+        east.setLayout(new GridLayout(3,1));
+        east.add(firstNode);
+        east.add(secondNode);
+        east.add(connected);
+        connected.addActionListener(this);
+        frame.add(east, BorderLayout.EAST);
+
+        panel.addMouseListener(this);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GraphCreator();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    //What happens when the mouse is released
+    public void mouseReleased(MouseEvent e) {
+        if (state == NODE_CREATE) {
+            panel.addNode(e.getX(), e.getY(), labelsTF.getText());
+        }
+        else if (state == EDGE_FIRST) {
+            Node n = panel.getNode(e.getX(), e.getY());
+            if (n != null) {  
+                first = n;
+                state = EDGE_SECOND; 
+                n.setHighlighted(true);
+            }
+        }
+        else if (state == EDGE_SECOND) {
+            Node n = panel.getNode(e.getX(), e.getY());
+            if (n != null && !first.equals(n)) {
+                String s = labelsTF.getText();
+                boolean valid = true;
+                for (int i = 0; i < s.length(); i++) {
+                    if (Character.isDigit(s.charAt(i)) == false) {
+                        valid = false;
+                    }
+                }
+                if (valid == true) {
+                    first.setHighlighted(false);
+                    panel.addEdge(first, n, labelsTF.getText());
+                    first = null;
+                    state = EDGE_FIRST;
+                }
+                else {
+                    JOptionPane.showMessageDialog(frame , "Can only have digits in edge label.");
+                }
+            }
+        }
+        frame.repaint();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    //What happens when a button is clicked
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(nodeB)) {
+            nodeB.setBackground(Color.GREEN);
+            edgeB.setBackground(Color.LIGHT_GRAY);
+            state = NODE_CREATE;
+        }
+        if (e.getSource().equals(edgeB)) {
+            edgeB.setBackground(Color.GREEN);
+            nodeB.setBackground(Color.LIGHT_GRAY);
+            state = EDGE_FIRST;
+            panel.stopHighlighting();
+            frame.repaint();
+        }
+        if (e.getSource().equals(connected)) {
+            if (panel.nodeExists(firstNode.getText()) == false) {
+                JOptionPane.showMessageDialog(frame , "First node is not in you graph.");
+            }
+            else if (panel.nodeExists(secondNode.getText()) == false) {
+                JOptionPane.showMessageDialog(frame , "Second node is not in you graph.");
+            }
+            else {
+                Queue queue = new Queue();
+                ArrayList<String> connectedList = new ArrayList<String>();
+                connectedList.add(panel.getNode(firstNode.getText()).getLabel());
+                ArrayList<String> edges = panel.getConnectedLabels(firstNode.getText());
+                for (int i = 0; i < edges.size(); i++) {
+                    queue.endQueue(edges.get(i));
+                 }
+                while (queue.isEmpty() == false) {
+                   String currentNode = queue.deQueue();
+                   if (connectedList.contains(currentNode) == false) {
+                       connectedList.add(currentNode);
+                   }
+                   edges = panel.getConnectedLabels(currentNode);
+                   for (int i = 0; i < edges.size(); i++) {
+                       if (connectedList.contains(edges.get(i)) == false) {
+                           queue.endQueue(edges.get(i));
+                       }
+                   }
+                }
+                if (connectedList.contains(secondNode.getText())) {
+                    JOptionPane.showMessageDialog(frame , "Connected");
+                }
+                else {
+                    JOptionPane.showMessageDialog(frame , "Not Connected");
+                }
+            }
+        }
+        if (e.getSource().equals(salesmanB)) {
+            if (panel.getNode(salesmanStartTF.getText()) != null) {
+                travelling(panel.getNode(salesmanStartTF.getText()), new ArrayList<Node>(), 0);
+                if (completed.size() != 0) {
+                    int cheapest = totalList.get(0);
+                    int cheapestPath = 0;
+                    int current;
+                    for (int i = 0; i < totalList.size(); i++) {
+                        current = totalList.get(i);
+                        if (current < cheapest) {
+                            cheapest = current;
+                            cheapestPath = i;
+                        }
+                    }
+                    ArrayList<Node> cheapestFinal = new ArrayList<Node>();
+					cheapestFinal.addAll(completed.get(cheapestPath));
+                    System.out.print("Cheapest Path: ");
+                    for (int i = 0; i < cheapestFinal.size(); i++) {
+                        System.out.print(cheapestFinal.get(i).getLabel() + ", ");
+                    }
+                    System.out.println("");
+                    System.out.println("Total Cost: " + cheapest);
+                }
+            }
+            else {
+                JOptionPane.showMessageDialog(frame , "Not a valid starting Node!");
+            }
+        }
+    }
+
+    public void travelling(Node n, ArrayList<Node> path, int total) {
+        ArrayList<Edge> edgeList = panel.getEdgeList();
+        if (path.size() == panel.getNodeList().size()) {//This checks if the path is completed
+            completed.add((ArrayList<Node>) path.clone());
+            totalList.add(total);
+            if (!path.isEmpty()) {
+                path.remove(path.size() - 1);
+            }
+            return;
+        }
+        else { 
+            for (int i = 0; i < edgeList.size(); i++) {//Goes through all the edges and checks if it is connected to the node the salesman is at
+                Edge e = edgeList.get(i);
+                if(e.getOtherEnd(n) != null) {
+                    if (path.contains(e.getOtherEnd(n)) == false) {
+                        path.add(e.getOtherEnd(n));
+                        travelling(e.getOtherEnd(n), path, total + Integer.parseInt(e.getLabel()));
+                    }
+                }
+            }
+            if (!path.isEmpty()) {
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+}
--- GraphPanel.java
+++ GraphPanel.java
@@ -1,165 +1,176 @@
+/* Created by: Nikaansh S.
+ * Last Edited: 5/12/23
+ * This class has draws the board along with the code for manipulation edges and nodes
+ */
+
+import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Iterator;
-
-import javax.swing.AbstractButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class GraphPanel extends JPanel {
-	
-	ArrayList<Node> nodeList =  new ArrayList<Node>();
-	ArrayList<Edge> edgeList = new ArrayList<Edge>();
-	int circleRadius = 20;
-	
-	ArrayList<ArrayList<Boolean>> ajm = new ArrayList<ArrayList<Boolean>>();
-	
-	public GraphPanel() {
-		super();
-	}
-	
-	public ArrayList<String> getConnectedLabels(String label){//8.41 Part 2 Creator 2
-		ArrayList<String> toReturn = new ArrayList<String>();
-		int b = getIndex(label);		
-		for (int i = 0; i < ajm.size(); i++) {
-			if ((ajm.get(b).get(i)== true) && (nodeList.get(i).getLabel().equals(label)== false)) {
-				toReturn.add(nodeList.get(i).getLabel());
-			}
-		}
-		return toReturn;
-	}
-	
-	
-	public void printAdjacency() {
-		System.out.println();
-		for (int i = 0; i < ajm.size(); i++) {
-			for (int j = 0; j <  ajm.get(0).size(); j++) {
-				System.out.print(ajm.get(i).get(j)+ "\t");
-			}
-			System.out.println();
-		}
-	}
-	public void addNode(int newx, int newy, String newlabel) {
-		nodeList.add(new Node(newx, newy, newlabel));
-		ajm.add(new ArrayList<Boolean>());
-		for (int i = 0; i < ajm.size()-1; i++) {
-			ajm.get(i).add(false);
-		}
-		for (int i = 0; i < ajm.size(); i++) {
-			ajm.get(ajm.size()-1).add(false);
-		}
-		printAdjacency();
-	}
-	
-	public void addEdge (Node first, Node second, String newlabel) {//12.02 part6
-		edgeList.add(new Edge(first,second, newlabel));
-		int findex = 0;
-		int sindex = 0;
-		for (int i = 0; i < nodeList.size(); i++) {
-			if (first.equals(nodeList.get(i))) {
-				findex=i;
-			}
-			if (second.equals(nodeList.get(i))) {
-				sindex=i;
-			}
-		}
-		ajm.get(findex).set(sindex, true);
-		ajm.get(sindex).set(findex, true);
-		printAdjacency();
-	}
-	
-	public Node getNode (int x, int y) {
-		for (int i = 0; i < nodeList.size(); i++) {
-			Node node = nodeList.get(i);
-			//Pythagorean theorem 
-			double radius = Math.sqrt(Math.pow(x-node.getX(), 2)+ Math.pow(y-node.getY(),2));
-			
-			if (radius < circleRadius) {
-				return node;
-			}
-		}
-		return null;
-	}
-	public Node getNode(String s) {
-		for (int i = 0; i < nodeList.size(); i++) {
-			Node node = nodeList.get(i);
-			if (s.equals(node.getLabel())) {
-				return node;
-			}
-		}
-		return null;
-	}
-	
-	public int getIndex(String s) {
-		for (int i = 0; i < nodeList.size(); i++) {
-			Node node = nodeList.get(i);
-			if (s.equals(node.getLabel())) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	
-	public boolean nodeExists(String s) {
-		for (int i = 0; i < nodeList.size(); i++) {
-			if (s.equals(nodeList.get(i).getLabel())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		//draw stuff
-		for (int i = 0; i < nodeList.size(); i++) {
-			if (nodeList.get(i).setHighlighted() == true) {
-				g.setColor(Color.red);
-			}
-			else {
-				g.setColor(Color.black);
-			}
-			g.drawOval(nodeList.get(i).getX()- circleRadius, nodeList.get(i).getY() - circleRadius, circleRadius*2, circleRadius*2);
-			g.drawString(nodeList.get(i).getLabel(),nodeList.get(i).getX()-3,nodeList.get(i).getY()+3);
-		}
-		for (int i = 0; i < edgeList.size(); i++) {
-			g.setColor(Color.BLACK);
-			g.drawLine(edgeList.get(i).getFirst().getX(), 
-						edgeList.get(i).getFirst().getY(), 
-						edgeList.get(i).getSecond().getX(), 
-						edgeList.get(i).getSecond().getY());
-			int fx = edgeList.get(i).getFirst().getX();
-			int fy = edgeList.get(i).getFirst().getY();
-			int sx = edgeList.get(i).getSecond().getX();
-			int sy = edgeList.get(i).getSecond().getY();
-			g.drawString(edgeList.get(i).getLabel(), ((Math.min(fx, sx)+ 
-					Math.max(sx, fx)) / 2),((Math.min(fy, sy)+ Math.max(sy,fy)) / 2));
-		}
-		
-	
-	}
-	public void stopHighlighting() {
-		for (int i = 0; i < nodeList.size(); i++) {
-			nodeList.get(i).setHighlighted(false);
-		}
-	}
-	public int getNodeCount() {
-		return nodeList.size();
-	}
-	public ArrayList<Edge> getEdges(Node n)
-	{
-		ArrayList<Edge> connectedEdges = new ArrayList<>();
-		for (Edge edge : edgeList) {
-			if(edge.connectsToNode(n)) {
-				connectedEdges.add(edge);
-			}
-		}
-		return connectedEdges;
-	}
-	
-	
-	
-	
+    
+    ArrayList<Node> nodeList = new ArrayList<Node>();
+
+    public ArrayList<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(ArrayList<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    ArrayList<Edge> edgeList = new ArrayList<Edge>();
+
+    public ArrayList<Edge> getEdgeList() {
+        return edgeList;
+    }
+
+    public void setEdgeList(ArrayList<Edge> edgeList) {
+        this.edgeList = edgeList;
+    }
+
+    ArrayList<ArrayList<Boolean>> adjacency = new ArrayList<ArrayList<Boolean>>();
+
+    int circleRadius = 20;
+
+    public GraphPanel() {
+        super();
+    }
+
+    //Returns the an arraylist of all lables right next to the node that is being searched
+    public  ArrayList<String> getConnectedLabels(String label) {
+        ArrayList<String> toReturn = new ArrayList<String>();
+        int b = getIndex(label);
+        for (int i = 0; i < adjacency.size(); i++) {
+            if (adjacency.get(b).get(i) == true && !nodeList.get(i).getLabel().equals(label)) {
+                 toReturn.add(nodeList.get(i).getLabel());   
+            }
+        }
+        return toReturn;
+    }
+
+    //Gets coordinated and outputs where the node is located
+    public Node getNode(int x, int y) {
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node node = nodeList.get(i); 
+            //a squared plus b squared= c squared
+            double radius = Math.sqrt(Math.pow(x - node.getX(), 2) + Math.pow(y - node.getY(), 2));
+            if (radius < circleRadius) {
+                return node;
+            }   
+        }
+        return null;
+    }
+
+    //This gets the node matching the string
+    public Node getNode(String s) {
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node n = nodeList.get(i);
+            if (s.equals(n.getLabel())) {
+                return n;
+            }
+        }
+        return null;
+    }
+
+    //Gets the index values of the node
+    public int getIndex(String s) {
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node n = nodeList.get(i);
+            if (s.equals(n.getLabel())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Checks if the nodein question exists
+    public boolean nodeExists(String s) {
+        for (int i = 0; i < nodeList.size(); i++) {
+            if (s.equals(nodeList.get(i).getLabel())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //adds an edge to the board
+    public void addEdge (Node first, Node second, String newLabel) {
+        edgeList.add(new Edge(first, second, newLabel));
+        int firstIndex = 0;
+        int secondIndex = 0;
+        for (int i = 0; i < adjacency.size(); i++) {
+            if (first.equals(nodeList.get(i))) {
+                firstIndex = i;
+            }
+            if (second.equals(nodeList.get(i))) {
+                secondIndex = i;
+            }
+        }
+        adjacency.get(firstIndex).set(secondIndex, true);
+        adjacency.get(secondIndex).set(firstIndex, true);
+        printAdjacency();
+    }
+
+    //adds a node to the board
+    public void addNode(int newx, int newy, String newLable) {
+        nodeList.add(new Node(newx, newy, newLable));
+        adjacency.add(new ArrayList<Boolean>());
+        for (int i = 0; i < adjacency.size(); i++) {
+            adjacency.get(i).add(false);
+        }
+        for (int i = 0; i < adjacency.size(); i++) {
+            adjacency.get(adjacency.size() - 1).add(false);
+        }
+        printAdjacency();
+    }
+
+    //prints out the adjecent matrix
+    public void printAdjacency() {
+        System.out.println();
+        for (int i = 0; i < adjacency.size(); i++) {
+            for (int j = 0; j < adjacency.size(); j++) {
+                System.out.print(adjacency.get(i).get(j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    //paints the board
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //draw my stuff
+        for (int i = 0; i < nodeList.size(); i++) {
+            if (nodeList.get(i).getHighlighted() == true) {
+                g.setColor(Color.RED);
+            }
+            else {
+                g.setColor(Color.BLACK);
+            }
+            g.drawOval(nodeList.get(i).getX() - circleRadius, nodeList.get(i).getY() - circleRadius, circleRadius * 2, circleRadius * 2);
+            g.drawString(nodeList.get(i).getLabel(), nodeList.get(i).getX() - 10, nodeList.get(i).getY() - 10);
+        }
+        for (int i = 0; i < edgeList.size(); i++) {
+            g.setColor(Color.BLACK);
+            if (edgeList.get(i).getFirst().getX() < edgeList.get(i).getSecond().getX()) {
+                g.drawLine(edgeList.get(i).getFirst().getX() + circleRadius, edgeList.get(i).getFirst().getY(), edgeList.get(i).getSecond().getX() - circleRadius, edgeList.get(i).getSecond().getY());
+            }
+            else if (edgeList.get(i).getFirst().getX() > edgeList.get(i).getSecond().getX()) {
+                g.drawLine(edgeList.get(i).getFirst().getX() - circleRadius, edgeList.get(i).getFirst().getY(), edgeList.get(i).getSecond().getX() + circleRadius, edgeList.get(i).getSecond().getY());
+            }
+            int fx = edgeList.get(i).getFirst().getX();
+            int fy = edgeList.get(i).getFirst().getY();
+            int sx = edgeList.get(i).getSecond().getX();
+            int sy = edgeList.get(i).getSecond().getY();
+            g.drawString(edgeList.get(i).getLabel(), Math.min(fx, sx) + (Math.abs(sx-fx) / 2), Math.min(fy, sy) + (Math.abs(sy-fy) / 2));
+        }
+    }
+
+    //stop highlighting a node
+    public void stopHighlighting() {
+        for (int i = 0; i < nodeList.size(); i++) {
+            nodeList.get(i).setHighlighted(false);
+        }
+    }
 }
